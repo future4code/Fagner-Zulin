6b0455f1779bffb3ac5d84cfc3babef973174474
Fagner Zulin <fagnerzulin11@gmail.com>
2021-02-26 13:35:20 -0300

    Finalizado o desafio

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -1,13 +1,91 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+let cartaUsuario = [];
+let placarUsuario = 0;
 
+let cartaComputador = [];
+let placarComputador = 0;
+
+console.log("Bem vindo ao jogo de Blackjack!");
+
+if (confirm("Quer iniciar uma nova rodada?")) {
+  let flagAsesIguais = true;
+  while (flagAsesIguais) {
+    for (let index = 0; index < 2; index++) {
+      const carta = comprarCarta();
+      cartaUsuario.push(carta.texto);
+      placarUsuario += carta.valor;
+    }
+
+    for (let index = 0; index < 2; index++) {
+      const carta = comprarCarta();
+      cartaComputador.push(carta.texto);
+      placarComputador += carta.valor;
+    }
+    if (
+      (cartaUsuario[0].includes("A") && cartaUsuario[1].includes("A")) ||
+      (cartaComputador[0].includes("A") && cartaComputador[1].includes("A"))
+    ) {
+      cartaComputador = [];
+      cartaUsuario = [];
+      placarComputador = 0;
+      placarUsuario = 0;
+    } else {
+      flagAsesIguais = false;
+    }
+  }
+
+  let flagCompraMaisCarta = true;
+  while (flagCompraMaisCarta) {
+    const comprarMaiscarta = confirm(
+      "Suas cartas são " +
+        cartaUsuario.join(" ") +
+        ". A carta revelada do computador é " +
+        cartaComputador[0] +
+        "\n" +
+        "Deseja comprar mais uma carta?"
+    );
+
+    if (placarUsuario >= 21) {
+      flagCompraMaisCarta = false;
+    } else if (comprarMaiscarta) {
+      const carta = comprarCarta();
+      cartaUsuario.push(carta.texto);
+      placarUsuario += carta.valor;
+    } else {
+      flagCompraMaisCarta = comprarMaiscarta;
+
+      if (placarUsuario <= 21) {
+        while (placarComputador <= placarUsuario) {
+          const carta = comprarCarta();
+          cartaComputador.push(carta.texto);
+          placarComputador += carta.valor;
+        }
+      }
+    }
+  }
+
+  let resultado;
+
+  if (placarComputador === placarUsuario) {
+    resultado = "Empate!";
+  } else if (placarComputador > 21 && placarUsuario < 21) {
+    resultado = "O usuário ganhou!";
+  } else if (placarUsuario > 21) {
+    resultado = "O computador ganhou!";
+  }
+
+  alert(
+    "Suas cartas são " +
+      cartaUsuario.join(" ") +
+      ". Sua pontuação é " +
+      placarUsuario +
+      ".\n" +
+      "As cartas do computador são " +
+      cartaComputador.join(" ") +
+      ". A pontuação do computador é " +
+      placarComputador +
+      ".\n" +
+      resultado
+  );
+} else {
+  console.log("O jogo acabou");
+}
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,49 +1,49 @@
-const cartaUsuario = [];
-let placarUsuario = 0;
+// const cartaUsuario = [];
+// let placarUsuario = 0;
 
-const cartaComputador = [];
-let placarComputador = 0;
+// const cartaComputador = [];
+// let placarComputador = 0;
 
-console.log("Bem vindo ao jogo de Blackjack!");
+// console.log("Bem vindo ao jogo de Blackjack!");
 
-if (confirm("Quer iniciar uma nova rodada?")) {
-  for (let index = 0; index < 2; index++) {
-    const carta = comprarCarta();
-    cartaUsuario.push(carta.texto);
-    placarUsuario += carta.valor;
-  }
+// if (confirm("Quer iniciar uma nova rodada?")) {
+//   for (let index = 0; index < 2; index++) {
+//     const carta = comprarCarta();
+//     cartaUsuario.push(carta.texto);
+//     placarUsuario += carta.valor;
+//   }
 
-  for (let index = 0; index < 2; index++) {
-    const carta = comprarCarta();
-    cartaComputador.push(carta.texto);
-    placarComputador += carta.valor;
-  }
+//   for (let index = 0; index < 2; index++) {
+//     const carta = comprarCarta();
+//     cartaComputador.push(carta.texto);
+//     placarComputador += carta.valor;
+//   }
 
-  console.log(
-    "Usuário - cartas: " +
-      cartaUsuario[0] +
-      " " +
-      cartaUsuario[1] +
-      " - pontuação " +
-      placarUsuario
-  );
+//   console.log(
+//     "Usuário - cartas: " +
+//       cartaUsuario[0] +
+//       " " +
+//       cartaUsuario[1] +
+//       " - pontuação " +
+//       placarUsuario
+//   );
 
-  console.log(
-    "Computador - cartas: " +
-      cartaComputador[0] +
-      " " +
-      cartaComputador[1] +
-      " - pontuação " +
-      placarComputador
-  );
+//   console.log(
+//     "Computador - cartas: " +
+//       cartaComputador[0] +
+//       " " +
+//       cartaComputador[1] +
+//       " - pontuação " +
+//       placarComputador
+//   );
 
-  if (placarComputador === placarUsuario) {
-    console.log("Empate!");
-  } else if (placarUsuario > placarComputador) {
-    console.log("O usuário ganhou!");
-  } else {
-    console.log("O computador ganhou!");
-  }
-} else {
-  console.log("O jogo acabou");
-}
+//   if (placarComputador === placarUsuario) {
+//     console.log("Empate!");
+//   } else if (placarUsuario > placarComputador) {
+//     console.log("O usuário ganhou!");
+//   } else {
+//     console.log("O computador ganhou!");
+//   }
+// } else {
+//   console.log("O jogo acabou");
+// }
