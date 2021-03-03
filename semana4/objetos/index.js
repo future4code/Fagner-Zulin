0d3bc75901476e6276929a9f657c3a74320817e0
Fagner Zulin <fagnerzulin11@gmail.com>
2021-03-03 14:44:43 -0300

    finalizado a atividade básica do dia

--- semana4/objetos/index.js
+++ semana4/objetos/index.js
@@ -1,11 +1,20 @@
 const colecaoDePosts = [];
 
-function capturaInputs() {
-  const inputTitulo = document.getElementById("titulo-post");
-  const inputAutor = document.getElementById("autor-post");
-  const inputConteudo = document.getElementById("conteudo-post");
+function criaPosts() {
+  const { inputTitulo, inputAutor, inputConteudo } = capturaInputs();
 
-  if (validaCampos(inputTitulo.value, inputAutor.value, inputConteudo.value)) {
+  const valorTitulo = inputTitulo.value;
+  const valorAutor = inputAutor.value;
+  const valorConteudo = inputConteudo.value;
+
+  if (validaCampos(valorTitulo, valorAutor, valorConteudo)) {
+    colecaoDePosts.push(fabricaDePosts(valorTitulo, valorAutor, valorConteudo));
+
+    LimpaCampos([inputTitulo, inputAutor, inputConteudo]);
+
+    rederizaPost();
+  } else {
+    alert("Por Favor, preencha todos os campos.");
   }
 }
 
@@ -25,4 +34,34 @@ function LimpaCampos(arrayDeCampos) {
 
 function validaCampos(valorTitulo, valorAutor, valorConteudo) {
   return valorTitulo !== "" && valorAutor !== "" && valorConteudo !== "";
+}
+
+function capturaInputs() {
+  const inputTitulo = document.getElementById("titulo-post");
+  const inputAutor = document.getElementById("autor-post");
+  const inputConteudo = document.getElementById("conteudo-post");
+
+  return {
+    inputTitulo,
+    inputAutor,
+    inputConteudo,
+  };
+}
+
+function rederizaPost() {
+  const containerPosts = document.getElementById("container-de-posts");
+  limpaPost(containerPosts);
+  for (const post of colecaoDePosts) {
+    containerPosts.innerHTML += `
+    <div class="post">
+        <h1 class="titulo">${post.titulo}</h1>
+        <p class="conteudo">${post.conteudo}</p>
+        <p class="autor">${post.autor}</p>
+    </div>
+    `;
+  }
+}
+
+function limpaPost(container) {
+  container.innerHTML = "";
 }
