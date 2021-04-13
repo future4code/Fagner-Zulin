8b09eab186215f7109e1c71fe19201d2a237902d
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-13 14:42:48 -0300

    Add coordinates & Tested the useHistory

--- semana9/labex/src/pages/HomePage/HomePage.js
+++ semana9/labex/src/pages/HomePage/HomePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { gotToListTripPage, gotToLoginPage } from '../../routers/coordinates';
 
 export default function HomePage() {
+  const history = useHistory();
+
   return (
     <div>
       <p>Home</p>
+      <button type="button" onClick={() => gotToListTripPage(history)}>
+        Viagens
+      </button>
+      <button type="button" onClick={() => gotToLoginPage(history)}>
+        Login
+      </button>
     </div>
   );
 }
