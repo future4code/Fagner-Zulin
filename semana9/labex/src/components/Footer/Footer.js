b1c71049928e8d3dab6904cd4e21ecaecfc7677c
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-13 16:13:48 -0300

    Continuing to do the footer

--- semana9/labex/src/components/Footer/Footer.js
+++ semana9/labex/src/components/Footer/Footer.js
@@ -1,3 +1,9 @@
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 // eslint-disable-next-line import/no-unresolved
@@ -9,15 +15,54 @@ const FooterContainer = styled.footer`
   display: flex;
   align-items: center;
   padding: 20px;
+  justify-content: space-between;
 `;
 const Logo = styled.img`
   max-height: 100px;
+`;
+
+const PlanetsContainer = styled.div``;
+
+const SocialNetworkContainer = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .icones {
+    font-size: 1.5em;
+    cursor: pointer;
+  }
+  .icones:hover {
+    color: #7f8c8d;
+  }
 `;
 
 export default function Footer() {
   return (
     <FooterContainer>
       <Logo src={labexWhite} alt="LabeX" />
+      <PlanetsContainer>
+        <ul>
+          <li>Mercúrio</li>
+          <li>Vênus</li>
+          <li>Marte</li>
+          <li>Júpiter</li>
+        </ul>
+      </PlanetsContainer>
+      <PlanetsContainer>
+        <ul>
+          <li>Saturno</li>
+          <li>Urano</li>
+          <li>Netuno</li>
+          <li>Plutão</li>
+        </ul>
+      </PlanetsContainer>
+      <SocialNetworkContainer>
+        <FontAwesomeIcon className="icones" icon={faFacebookSquare} />
+        <FontAwesomeIcon className="icones" icon={faTwitterSquare} />
+        <FontAwesomeIcon className="icones" icon={faInstagramSquare} />
+      </SocialNetworkContainer>
     </FooterContainer>
   );
 }
