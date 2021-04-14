d6bc5a19ad5e0761807fe0b634090bb2b34b7774
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-14 14:22:05 -0300

    Started the LoginPage

--- semana9/labex/src/pages/LoginPage/LoginPage.js
+++ semana9/labex/src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import ContentContainer from '../../components/StyledComponentes/ContentContainer.styled';
+import PageContainer from '../../components/StyledComponentes/PageContainer.styled';
+
+const ContainerLogin = styled.section`
+  height: 70vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const FormLoginContainer = styled.div`
+  width: 50%;
+  height: 100%;
+`;
 
 export default function LoginPage() {
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <PageContainer background="#000">
+      <Header />
+      <ContentContainer>
+        <ContainerLogin>
+          <FormLoginContainer>
+            <h2>Login</h2>
+          </FormLoginContainer>
+        </ContainerLogin>
+      </ContentContainer>
+      <Footer />
+    </PageContainer>
   );
 }
