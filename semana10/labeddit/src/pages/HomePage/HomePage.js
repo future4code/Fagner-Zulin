05f8d781d7f3f2fed24cad52ecfdc8f016afa868
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-27 09:19:24 -0300

    Validate token homepage

--- semana10/labeddit/src/pages/HomePage/HomePage.js
+++ semana10/labeddit/src/pages/HomePage/HomePage.js
@@ -1,19 +1,31 @@
 import { Button } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import ContainerPage from '../../components/StyledComponents/PageContainer';
 import Title from '../../components/StyledComponents/Title';
-import { gotToLoginPage } from '../../routers/coordinate';
+import { gotToFeedPage, gotToLoginPage } from '../../routers/coordinate';
 import { ContainerHome, Text } from './homePage.styled';
+import { hasToken } from '../../utils/localStorageFunctions';
 
 export default function HomePage() {
   const history = useHistory();
+  const [hasTokenValue, setHasTokenValue] = useState(false);
+
+  useEffect(() => {
+    setHasTokenValue(hasToken());
+  }, []);
+
   return (
     <ContainerPage>
       <ContainerHome>
         <Title>LabEddit</Title>
         <Text>Fale sobre o que você quiser com o mundo!</Text>
-        <Button onClick={() => gotToLoginPage(history)} colorScheme="cyan">
+        <Button
+          onClick={() =>
+            hasTokenValue ? gotToFeedPage(history) : gotToLoginPage(history)
+          }
+          colorScheme="cyan"
+        >
           Clique aqui para começar
         </Button>
       </ContainerHome>
