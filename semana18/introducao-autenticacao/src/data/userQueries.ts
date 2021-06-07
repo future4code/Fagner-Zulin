c9d2dfcce6840b0a6b1642055eb309cdc3717154
Fagner Zulin <fagnerzulin11@gmail.com>
2021-06-07 17:13:43 -0300

    Done exercise 5

--- semana18/introducao-autenticacao/src/data/userQueries.ts
+++ semana18/introducao-autenticacao/src/data/userQueries.ts
@@ -4,3 +4,8 @@ import knexConnection from "./connection";
 export const createUser = async (data: User): Promise<void> => {
   await knexConnection("User").insert(data);
 };
+
+export const selectUserBy = async (email: string): Promise<any> => {
+  const [result] = await knexConnection("User").select().where({ email });
+  return result;
+};
