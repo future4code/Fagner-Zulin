5dabe810d00939bc87dd3895bcbb54dc380330b7
Fagner Zulin <fagnerzulin11@gmail.com>
2021-06-13 13:36:41 -0300

    Extracts followsController from userController

--- semana18/cookenu/src/controllers/userController.ts
+++ semana18/cookenu/src/controllers/userController.ts
@@ -1,17 +1,8 @@
 import { Request, Response } from "express";
-import { UserFollow } from "../types/user";
-import {
-  deleteFollow,
-  dropUser,
-  getFeed,
-  insertFollow,
-  selectUserById,
-} from "../data/userQueries";
+import { dropUser, getFeed, selectUserById } from "../data/userQueries";
 import { tokenValidator } from "../services/tokenService";
 import CustomError from "../errors/customError";
 import { hasHeaderToken } from "../validations/validHeaderToken";
-import { validFollowField } from "../validations/validFieldFollow";
-import { validUnfollowField } from "../validations/validFieldUnfollow";
 import { formatData } from "../util/transformData";
 import { FeedData } from "../types/feed";
 
@@ -43,44 +34,6 @@ export default class UserController {
       const { id, name, email } = result;
 
       res.send({ id, name, email });
-    } catch ({ code, message }) {
-      res.status(code ? code : 400).send({ message });
-    }
-  };
-
-  follow = async (req: Request, res: Response) => {
-    try {
-      const token = hasHeaderToken(req.headers);
-      const { id: follower } = tokenValidator(token);
-      const followed = await validFollowField(req.body);
-
-      const data: UserFollow = {
-        followed_id: followed,
-        follower_id: follower,
-      };
-
-      await insertFollow(data);
-
-      res.send({ message: "Followed" });
-    } catch ({ code, message }) {
-      res.status(code ? code : 400).send({ message });
-    }
-  };
-
-  unfollow = async (req: Request, res: Response) => {
-    try {
-      const token = hasHeaderToken(req.headers);
-      const { id: follower } = tokenValidator(token);
-      const followed = await validUnfollowField(req.body);
-
-      const data: UserFollow = {
-        followed_id: followed,
-        follower_id: follower,
-      };
-
-      await deleteFollow(data);
-
-      res.send({ message: "Unfollowed" });
     } catch ({ code, message }) {
       res.status(code ? code : 400).send({ message });
     }
--- semana18/cookenu/src/routes.ts
+++ semana18/cookenu/src/routes.ts
@@ -2,11 +2,13 @@ import express, { Router } from "express";
 import UserController from "./controllers/userController";
 import RecipeController from "./controllers/recipeController";
 import AuthenticationUserController from "./controllers/authenticationUserController";
+import FollowsController from "./controllers/followsController";
 
 const routes: Router = express.Router();
 const userController = new UserController();
 const recipeController = new RecipeController();
 const authenticationUserController = new AuthenticationUserController();
+const followsController = new FollowsController();
 
 routes.get("/ping", (_, res) => {
   res.send({ message: "pong" });
@@ -16,10 +18,12 @@ routes.post("/signup", authenticationUserController.signup);
 routes.post("/login", authenticationUserController.login);
 routes.put("/reset", authenticationUserController.resetPassword);
 
-routes.get("/user/profile", userController.getProfile);
-routes.post("/user/follow", userController.follow);
-routes.post("/user/unfollow", userController.unfollow);
+routes.post("/user/follow", followsController.follow);
+routes.post("/user/unfollow", followsController.unfollow);
+
 routes.get("/user/feed", userController.feed);
+
+routes.get("/user/profile", userController.getProfile);
 routes.get("/user/:id", userController.getProfileById);
 routes.delete("/user/:id", userController.deleteUser);
 
