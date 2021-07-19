701040f5562332324e75a3127708ec652723056c
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-19 20:03:44 -0300

    Done exercise 1 and 2

--- semana24/resolvendo-problemas/src/index.ts
+++ semana24/resolvendo-problemas/src/index.ts
@@ -2,10 +2,10 @@ import oneEditAway from "./exercicio1";
 import getCharAndCount from "./exercicio2";
 
 //Exercicio 1
-// console.log(oneEditAway("banan", "banana"));
-// console.log(oneEditAway("bananak", "banana"));
-// console.log(oneEditAway("banara", "banana"));
-// console.log(oneEditAway("bananaaa", "banana"));
+console.log(oneEditAway("banan", "banana"));
+console.log(oneEditAway("bananak", "banana"));
+console.log(oneEditAway("banara", "banana"));
+console.log(oneEditAway("bananaaa", "banana"));
 
 //Exercicio 2
 console.log(getCharAndCount("aabbb"));
